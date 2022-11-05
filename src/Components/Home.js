3a5ff727b35d2adcf871e82f92532772be664d24
Kingsley Sado <kinglogomhe@gmail.com>
2022-11-06 00:31:53 +0100

    added seo to landing page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Seo from "./Seo";
 
 const Home = () => {
-  return (
-    <div> 
-        <h1>Welcome... This is the Homepage for my ALT-SCHOOL counter app project....</h1>
+    return (
+        <div>
+            <Seo
+                title= 'A React Counter App project'
+                name= 'description'
+              description= 'A Beginner friendly react app project'
+            type='article'
+              />
+            <h1>Welcome... This is the Homepage for my ALT-SCHOOL counter react app project....</h1>
         </div>
-  )
+    )
 }
 
 export default Home
